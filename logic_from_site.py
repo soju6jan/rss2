082528a34f2a07213492d9d34d9d684e6f3aee7a
soju6jan <cybersol@naver.com>
2020-03-08 17:47:23 +0900

    1

--- logic_from_site.py
+++ logic_from_site.py
@@ -30,7 +30,8 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
     'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Language' : 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Referer' : ''
+    'Referer' : '',
+    'Cookie' :'over18=1'
 } 
 
 
